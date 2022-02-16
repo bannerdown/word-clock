06c1f0291f631e8131ecd6cae80f49aaaa626350
bannerdown <98526637+bannerdown@users.noreply.github.com>
2022-02-16 15:14:59 +0000

    Update and rename Credentials_example.h to Credentials.h

---
+++ arduino-cayenne/word_clock_cayenne/Credentials.h
@@ -0,0 +1,8 @@
+// Wifi settings
+char ssid[] = "Conkers back room";
+char wifiPassword[] = "D051917C20";
+
+// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
+char username[] = "d0d4c0b0-810b-11ec-8da3-474359af83d7";
+char password[] = "335f182226ecff48689dd235f318bb48b3cc2108";
+char clientID[] = "684c62b0-899d-11ec-9f5b-45181495093e";
--- arduino-cayenne/word_clock_cayenne/Credentials_example.h
+++
@@ -1,8 +0,0 @@
-// Wifi settings
-char ssid[] = "";
-char wifiPassword[] = "";
-
-// Cayenne authentication info. This should be obtained from the Cayenne Dashboard.
-char username[] = "";
-char password[] = "";
-char clientID[] = "";
